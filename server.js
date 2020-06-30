2f813cceac70c055cd756e6f3b4617b0c148bb99
Nate <nate@robinhoodpro.com>
2020-06-30 15:47:43 +0300

    Remove failing server method

--- server.js
+++ server.js
@@ -143,13 +143,4 @@ app.listen(port);
 
 console.log('Express listening on port ' + port);
 
-//Open browser
-if (!inContainer && !inAzure) {
-    var opn = require('opn');
-    
-    opn('http://localhost:' + port).then(() => {
-        console.log('Browser closed.');
-    });
-}
-
 
